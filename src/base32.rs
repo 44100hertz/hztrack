f6855360fb68e0d313020ad5a7aad9c62a36dea1
Samuel Pagenkopf <ssaammp@gmail.com>
2017-08-09 06:08:47 -0500

    removed redundant conversions

--- src/base32.rs
+++ src/base32.rs
@@ -1,14 +1,13 @@
 // 0123456789ABCDEFGHJKLMNPQRTUVWXY
 
 pub fn from_char(c: char) -> u8 {
-    let wide: char = match c {
-        '0' | 'O' | 'o' => '0',
-        '1' | 'I' | 'l' | 'i' => '1',
-        '2' | 'Z' | 'z' => '2',
-        '5' | 'S' | 's' => '5',
-        '6' | 'b' => '6',
-        '8' | 'B' => '8',
-        '9' | 'g' => '9',
+    let wide = match c {
+        'O' | 'o' => '0',
+        'I' | 'l' | 'i' => '1',
+        'Z' | 'z' => '2',
+        'S' | 's' => '5',
+        'b' => '6',
+        'g' => '9',
         _ => c.to_uppercase().nth(0).unwrap(),
     };
     wide as u8
