3187a1d1e9f188e01d25ae8c8c3a15d3cef7feaa
Sam Pagenkopf <ssaammp@gmail.com>
2017-10-09 01:35:32 -0500

    drawing text

--- src/display/mod.rs
+++ src/display/mod.rs
@@ -1,6 +1,40 @@
 extern crate sdl2;
 use sdl2::pixels::Color;
 
+use sdl2::render::*;
+use sdl2::video::Window;
+use sdl2::rect::*;
+
+pub struct Artist<'tex> {
+    canvas: Canvas<Window>,
+    font: Texture<'tex>,
+    scale: u32,
+}
+
+impl<'tex> Artist<'tex> {
+    fn clear(&mut self) {
+        self.canvas.set_draw_color(Color{r:0, g:0, b:255, a:255});
+        self.canvas.clear();
+    }
+    fn present(&mut self) {
+        self.canvas.present();
+    }
+    fn write(&mut self, mut x: i32, y: i32, chars: &str) {
+        const CHAR_W: i32 = 8;
+        const CHAR_H: i32 = 8;
+        for c in chars.as_bytes() {
+            let src = Rect::new(
+                (*c as i32 % 16) * CHAR_W,
+                (*c as i32 / 16) * CHAR_H,
+                CHAR_W as u32,
+                CHAR_H as u32);
+            let dest = Rect::new(x, y, CHAR_W as u32 * self.scale, CHAR_H as u32 * self.scale);
+            self.canvas.copy(&self.font, Some(src), Some(dest)).unwrap();
+            x += CHAR_W * self.scale as i32;
+        }
+    }
+}
+
 pub fn run(sdl: sdl2::Sdl) {
     let video_subsys = sdl.video().unwrap();
     let win = video_subsys.window("rusttracker", 800, 600)
@@ -14,8 +48,17 @@ pub fn run(sdl: sdl2::Sdl) {
         .target_texture()
         .build().unwrap();
     let tex_creator = canvas.texture_creator();
+
+    use sdl2::surface::Surface;
     let font = tex_creator.create_texture_from_surface(
-        sdl2::surface::Surface::load_bmp("res/font.bmp").unwrap()).unwrap();
+        Surface::load_bmp("res/font.bmp").unwrap())
+        .unwrap();
+
+    let mut artist = Artist{
+        canvas: canvas,
+        font: font,
+        scale: 3,
+    };
 
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
@@ -26,9 +69,8 @@ pub fn run(sdl: sdl2::Sdl) {
                 _ => {}
             }
         }
-        canvas.set_draw_color(Color{r:0, g:0, b:255, a:255});
-        canvas.clear();
-        canvas.copy(&font, None, None).unwrap();
-        canvas.present();
+        artist.clear();
+        artist.write(0, 0, "Hello, world!");
+        artist.present();
     }
 }
